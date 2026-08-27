4d94472a0e717d7dab61d1cbd53baee060caacc5
BulatKaramov <bulat.korsar@gmail.com>
2026-08-27 13:01:18 +0300

    fix syntax, user function parms

--- 4-navigator/distanceToAddress.js
+++ 4-navigator/distanceToAddress.js
@@ -3,8 +3,8 @@ const addressLong = 15;
 const positionLat = 0;
 const positionLong = 0;
 
-function distanceToObject (addLat, addLong, posLat, pos,Long) {
-    return Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2);
+function distanceToObject (addLat, addLong, posLat, posLong) {
+    return Math.sqrt((addLat - posLat) ** 2 + (addLong - posLong) ** 2);
 }
 
 console.log (`Расстояние до адреса = ${distanceToObject(addressLat, addressLong, positionLat, positionLong)}`);
