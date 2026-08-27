33883d18e881e7eefa89cfe7e8b7bbc21274db28
BulatKaramov <bulat.korsar@gmail.com>
2026-08-27 15:58:40 +0300

    5 Languages init

---
+++ 5 Languages/welcome.js
@@ -0,0 +1,13 @@
+ let language = 'RU';
+
+ switch (language) {
+    case 'RU':
+        console.log('Здравствуйте!');
+    case 'DE':
+        console.log('Gutten tag!');
+    case 'PL':
+        console.log('Cześć!');
+    case 'EN':
+    default:
+        console.log('Welcome!')
+ }
