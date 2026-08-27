057eb88d37ac13fb613c61780d27896e13438a90
BulatKaramov <bulat.korsar@gmail.com>
2026-08-27 16:00:20 +0300

    add breaks

--- 5 Languages/welcome.js
+++ 5 Languages/welcome.js
@@ -3,10 +3,13 @@
  switch (language) {
     case 'RU':
         console.log('Здравствуйте!');
+        break;
     case 'DE':
         console.log('Gutten tag!');
+        break;
     case 'PL':
         console.log('Cześć!');
+        break;
     case 'EN':
     default:
         console.log('Welcome!')
